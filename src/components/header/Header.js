f2a21311be49b1e5b09bfab985059e2b3530fb96
Danielle Silva <danielle.luisasilva@gmail.com>
2024-06-11 20:58:27 -0300

    create header

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <p>HOME</p>
+      <p>SOBRE</p>
+      <p>PROJETOS</p>
+      <p>CONTATO</p>
+      
+      <p>LANGUAGE SWITCH</p>
+
+    </header>
+  )
+}
